dda38fec21070fcfa7be700941567a28affc8f20
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2018-10-04 14:31:24 +0200

    add files

---
+++ constants.js
@@ -0,0 +1,5 @@
+//MAIN
+export const mnidAddress = '2oskpCiZwMmubvhn3M2YyMq3jL9Upr2MDua';
+export const signingKey = '64276b1f9d09a53e6ceaec65d6e8935f713f5354996853369a1b4e92168bbb37';
+export const appName = 'Gemeente Amstrecht';
+export const network = 'rinkeby';
---
+++ index.js
@@ -0,0 +1,60 @@
+import 'babel-polyfill'
+import * as constants from './constants'
+import Vue from 'vue/dist/vue.esm.js'
+
+const uportConnect = require('uport-connect');
+const qrcode = require('qrcode-terminal');
+
+const uport = new uportConnect.Connect(constants.appName, {
+    //uriHandler,
+    clientId: constants.mnidAddress,
+    network: constants.network,
+    signer: uportConnect.SimpleSigner(constants.signingKey)
+});
+
+let creds = null;
+let receiver = '';
+let dob = null;
+
+new Vue({
+    el: '#app',
+    data: {
+      user: null,
+    },
+    methods: {
+      login: function() {
+        let app = this
+        // Request credentials
+        uport.requestCredentials({
+            requested: ['name', 'avatar', 'address', 'country', 'publicKey', 'publicEncKey', 'DoB'],
+            notifications: true
+        }).then((credentials) => {
+            app.user = credentials
+            //console.log(credentials);
+            receiver = credentials.did;
+            creds = credentials;
+        }).then(() => {
+          if(creds.avatar.uri!=null){
+            document.getElementById("avatarImg").src=creds.avatar.uri;
+          }
+        })
+      },
+      logout: function() {
+        this.user = null
+      },
+      setDoB: function() {
+        let dobValue = document.getElementById("dob").value;
+        dob = dobValue;
+        document.getElementById("dobText").innerHTML="Date of Birth saved: " + dob;
+      },
+      issueDoB: function() {
+        if(dob!=null){
+          uport.attestCredentials({
+            sub: receiver,
+            claim: { 'DoB': dob },
+          })
+        }
+      }
+    },
+  })
+  
